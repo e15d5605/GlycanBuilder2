cb750791f731a357cf48fe497c4376803ed45c06
S.TSUCHIYA <e15d5605@gmail.com>
2020-08-12 16:28:22 +0900

    Modified source codes.

---
+++ main/java/org/glycoinfo/application/glycanbuilder/util/exchange/importer/TrivialNameConverter.java
@@ -0,0 +1,27 @@
+package org.glycoinfo.application.glycanbuilder.util.exchange.importer;
+
+import org.glycoinfo.GlycanFormatconverter.Glycan.Monosaccharide;
+import org.glycoinfo.WURCSFramework.wurcs.sequence2.GRES;
+
+public class TrivialNameConverter {
+
+    public TrivialNameConverter() {
+
+    }
+
+    public void start (GRES _gres) {
+        Monosaccharide mono = new Monosaccharide();
+
+        //parse anomeric state and position
+
+        //parse stereos from SkeletonCode
+
+        //parse ring size
+
+        //parse SuperClass
+
+        //parse modification
+
+        return;
+    }
+}
